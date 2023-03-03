20f371d749f96132fda51190d787208e1efd9568
Sương <suongphan37@gmail.com>
2023-03-03 09:47:04 +0700

    add left bar

--- social-media-fe/src/pages/Home/index.jsx
+++ social-media-fe/src/pages/Home/index.jsx
@@ -1,10 +1,13 @@
+import LeftBar from "../../components/LeftBar";
 import NewPost from "../../components/NewPost";
 import Post from "../../components/Post";
 
 function Home() {
   return (
     <div className="d-flex justify-content-center align-items-center">
-      <div className="col-lg-3">Left Bar</div>
+      <div className="col-lg-3">
+        <LeftBar />
+      </div>
       <div className="col-lg-6">
         <NewPost />
         <Post />
