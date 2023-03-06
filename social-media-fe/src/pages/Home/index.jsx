ed060fa199de240239ff282e5be34a073f55cb99
XuanTan <tanpham0147@gmail.com>
2023-03-06 17:19:48 +0700

    fix scss leftbar

--- social-media-fe/src/pages/Home/index.jsx
+++ social-media-fe/src/pages/Home/index.jsx
@@ -5,14 +5,14 @@ import Post from "../../components/Post";
 function Home() {
   return (
     <div className="d-flex justify-content-center">
-      <div className="col-lg-3">
+      <div className="col-lg-3 mt-3">
         <LeftBar />
       </div>
-      <div className="col-lg-6">
+      <div className="col-lg-6 mt-3">
         <NewPost />
         <Post />
       </div>
-      <div className="col-lg-3">Right Bar</div>
+      <div className="col-lg-3 mt-3">Right Bar</div>
     </div>
   );
 }
