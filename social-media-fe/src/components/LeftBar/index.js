f898505b7141b6c77dcd06b908a1c9bf226e8ac0
XuanTan <tanpham0147@gmail.com>
2023-03-09 00:12:39 +0700

    code-BE-login

--- social-media-fe/src/components/LeftBar/index.js
+++ social-media-fe/src/components/LeftBar/index.js
@@ -1,5 +1,77 @@
+import styles from "./LeftBar.module.scss";
+import classNames from "classnames/bind";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFeed,
+  faMessage,
+  faPlayCircle,
+  faUserGroup,
+  faBookBookmark,
+  faCircleQuestion,
+  faGraduationCap,
+  faHouse,
+  faUser,
+  faCalendarDay,
+} from "@fortawesome/free-solid-svg-icons";
+
+const cx = classNames.bind(styles);
+
 function LeftBar() {
-  return <>Left Bar</>;
+  return (
+    <div className={cx("sidebar")}>
+      <div className={cx("sidebar-list")}>
+        <ul>
+          <li className={cx("sidebar-item")}>
+            <FontAwesomeIcon icon={faHouse} color="#00CED1" />
+            <span className={cx("sidebar-text")}>Home page</span>
+          </li>
+          <li className={cx("sidebar-item")}>
+            <FontAwesomeIcon icon={faUser} color="blue" />
+            <span className={cx("sidebar-text")}>Profile</span>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <div className={cx("sidebar-list")}>
+          <ul>
+            <li className={cx("sidebar-item")}>
+              <FontAwesomeIcon icon={faFeed} />
+              <span className={cx("sidebar-text")}>Feed</span>
+            </li>
+            <li className={cx("sidebar-item")}>
+              <FontAwesomeIcon icon={faMessage} color="#8B008B" />
+              <span className={cx("sidebar-text")}>Chats</span>
+            </li>
+            <li className={cx("sidebar-item")}>
+              <FontAwesomeIcon icon={faPlayCircle} color="red" />
+              <span className={cx("sidebar-text")}>Videos</span>
+            </li>
+            <li className={cx("sidebar-item")}>
+              <FontAwesomeIcon icon={faUserGroup} color="rgb(40, 4, 148)" />
+              <span className={cx("sidebar-text")}>Groups</span>
+            </li>
+            <li className={cx("sidebar-item")}>
+              <FontAwesomeIcon icon={faBookBookmark} color="#1E90FF" />
+              <span className={cx("sidebar-text")}>Bookmarks</span>
+            </li>
+            <li className={cx("sidebar-item")}>
+              <FontAwesomeIcon icon={faCircleQuestion} color="#DAA520" />
+              <span className={cx("sidebar-text")}>Questions</span>
+            </li>
+            <li className={cx("sidebar-item")}>
+              <FontAwesomeIcon icon={faCalendarDay} color="#C71585" />
+              <span className={cx("sidebar-text")}>Events</span>
+            </li>
+            <li className={cx("sidebar-item")}>
+              <FontAwesomeIcon icon={faGraduationCap} color="#0000FF"/>
+              <span className={cx("sidebar-text")}>Courses</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LeftBar;
--- social-media-fe/src/pages/Register/index.jsx
+++ social-media-fe/src/pages/Register/index.jsx
@@ -1,6 +1,15 @@
+import {
+  message,
+} from "antd";
 import styles from "./Register.module.scss";
 import classNames from "classnames/bind";
-import { useState } from "react";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../context/AuthContext";
+//import useScreenSize from "../../hooks/useScreenSize";
+import { API } from "../../constant";
+import { setToken } from "../../helpers";
 
 const cx = classNames.bind(styles);
 
@@ -73,6 +82,47 @@ function Register() {
     }
   };
   console.log(fromError);
+  
+    const navigate = useNavigate();
+  
+    const { setUser } = useAuthContext();
+  
+    const [isLoading, setIsLoading] = useState(false);
+  
+    const [error, setError] = useState("");
+  
+    const onFinish = async (values) => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(`${API}/auth/local/register`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values),
+        });
+  
+        const data = await response.json();
+        if (data?.error) {
+          throw data?.error;
+        } else {
+          // set the token
+          setToken(data.jwt);
+  
+          // set the user
+          setUser(data.user);
+  
+          message.success(`Welcome to Social Cards ${data.user.username}!`);
+  
+          navigate("/profile", { replace: true });
+        }
+      } catch (error) {
+        console.error(error);
+        setError(error?.message ?? "Something went wrong!");
+      } finally {
+        setIsLoading(false);
+      }
+    };
   return (
     <div className={cx("form-register")}>
       <div className={cx("register")}>
@@ -226,4 +276,5 @@ function Register() {
     </div>
   );
 }
+
 export default Register;
