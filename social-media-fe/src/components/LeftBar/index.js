4baa3f78375e4d9a1adc45b86c10a532e2a50085
XuanTan <tanpham0147@gmail.com>
2023-03-08 09:56:53 +0700

    fix scss

--- social-media-fe/src/components/LeftBar/index.js
+++ social-media-fe/src/components/LeftBar/index.js
@@ -11,7 +11,6 @@ import {
   faHouse,
   faUser,
   faCalendarDay,
-  
 } from "@fortawesome/free-solid-svg-icons";
 //import { Users } from "../../dynamicData";
 //import CloseFriend from "../closeFriends/CloseFriend";
@@ -19,91 +18,91 @@ import {
 function LeftBar() {
   return (
     <div className="sidebar">
-      <div className="sidebarHomepage sidebar-list">
-        <ul className="sidebarHome">
-          <li className="sidebarListItem">
+      <div className="sidebar-Homepage sidebar-list">
+        <ul className="sidebar-Home">
+          <li className="sidebar-ListItem">
             <FontAwesomeIcon
               className="fa-solid fa-house"
               icon={faHouse}
               color="#00CED1"
             />
-            <span className="sidebarListItemText">Home page</span>
+            <span className="sidebar-ListItemText">Home page</span>
           </li>
-          <li className="sidebarListItem">
+          <li className="sidebar-ListItem">
             <FontAwesomeIcon
               className="fa-solid fa-user"
               icon={faUser}
               color="blue"
             />
-            <span className="sidebarListItemText">Profile</span>
+            <span className="sidebar-ListItemText">Profile</span>
           </li>
         </ul>
       </div>
       <div className="sidebar">
-        <div className="sidebarWrapper sidebar-list">
-          <ul className="sidebarList">
-            <li className="sidebarListItem">
+        <div className="sidebar-Wrapper sidebar-list">
+          <ul className="sidebar-List">
+            <li className="sidebar-ListItem">
               <FontAwesomeIcon className="fa-solid fa-rss" icon={faFeed} />
-              <span className="sidebarListItemText">Feed</span>
+              <span className="sidebar-ListItemText">Feed</span>
             </li>
-            <li className="sidebarListItem">
+            <li className="side-barListItem">
               <FontAwesomeIcon
                 className="fa-solid fa-message"
                 icon={faMessage}
                 color="#8B008B"
               />
-              <span className="sidebarListItemText">Chats</span>
+              <span className="sidebar-ListItemText">Chats</span>
             </li>
-            <li className="sidebarListItem">
+            <li className="side-barListItem">
               <FontAwesomeIcon
                 className="fa-solid fa-play"
                 icon={faPlayCircle}
                 color="red"
               />
-              <span className="sidebarListItemText">Videos</span>
+              <span className="sidebar-ListItemText">Videos</span>
             </li>
-            <li className="sidebarListItem">
+            <li className="sidebar-ListItem">
               <FontAwesomeIcon
                 className="fa-duotone fa-user-group"
                 icon={faUserGroup}
                 color="rgb(40, 4, 148)"
               />
-              <span className="sidebarListItemText">Groups</span>
+              <span className="sidebar-ListItemText">Groups</span>
             </li>
-            <li className="sidebarListItem">
+            <li className="sidebar-ListItem">
               <FontAwesomeIcon
                 className="fa-solid fa-bookmark"
                 icon={faBookBookmark}
                 color="#1E90FF"
               />
-              <span className="sidebarListItemText">Bookmarks</span>
+              <span className="sidebar-ListItemText">Bookmarks</span>
             </li>
-            <li className="sidebarListItem">
+            <li className="sidebar-ListItem">
               <FontAwesomeIcon
                 className="fa-sharp fa-solid fa-circle-question"
                 icon={faCircleQuestion}
                 color="#DAA520"
               />
-              <span className="sidebarListItemText">Questions</span>
+              <span className="sidebar-ListItemText">Questions</span>
             </li>
-            <li className="sidebarListItem">
+            <li className="sidebar-ListItem">
               <FontAwesomeIcon
                 className="fa-solid fa-calendar-days"
                 icon={faCalendarDay}
                 color="#C71585"
               />
-              <span className="sidebarListItemText">Events</span>
+              <span className="sidebar-ListItemText">Events</span>
             </li>
-            <li className="sidebarListItem">
+            <li className="sidebar-ListItem">
               <FontAwesomeIcon
                 className="fa-solid fa-graduation-cap"
                 icon={faGraduationCap}
                 color="#0000FF"
               />
-              <span className="sidebarListItemText">Courses</span>
+              <span className="sidebar-ListItemText">Courses</span>
             </li>
           </ul>
-     </div>
+        </div>
       </div>
     </div>
   );
