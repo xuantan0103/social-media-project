585c6d8d02f1e3231e5e8acf7617c45cacf70191
Sương <suongphan37@gmail.com>
2023-03-10 16:59:07 +0700

    fix friend page

--- social-media-fe/src/routes/routes.jsx
+++ social-media-fe/src/routes/routes.jsx
@@ -8,11 +8,24 @@ import Home from "../pages/Home";
 
 import routes from "../config/routes";
 import Profile from "../pages/Profile";
+import FriendRequests from "../pages/FriendRequests/FriendRequest";
+import FriendSent from "../pages/FriendRequests/FriendSent";
 const publicRoutes = [
   { path: routes.login, component: Login },
   { path: routes.register, component: Register },
   { path: routes.home, component: Home, layout: DefaultLayout },
   { path: routes.profile, component: Profile, layout: DefaultLayout },
+  {
+    path: routes.friendrequests,
+    component: FriendRequests,
+    layout: DefaultLayout,
+  },
+
+  {
+    path: routes.sentrequests,
+    component: FriendSent,
+    layout: DefaultLayout,
+  },
 ];
 
 const privateRoutes = [];
