a93928679fe4020e2ed7e160b1c11700bff6feec
XuanTan <tanpham0147@gmail.com>
2023-03-17 15:28:20 +0700

    code-editprofile

--- social-media-fe/src/pages/editProfile/EditProfile.jsx
+++ social-media-fe/src/pages/editProfile/EditProfile.jsx
@@ -1,56 +1,81 @@
 import "../editProfile/EditProfile.css"
-import React from "react";
-function editProfile(){
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope, faLocationDot, faPen, faPhone, faUser,
+} from "@fortawesome/free-solid-svg-icons";
+// import React, {useState} from 'react';
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+const editProfile = ({ user, onSave }) => {
+  // const [fullname, setFullname] = useState(user.fullname);
+  // const [email, setEmail] = useState(user.email);
+  // const [phone, setPhone] = useState(user.phone);
+  // const [address, setAddress] = useState(user.address);
 
-    return (
-        <>
-        <form
-          onSubmit={handleSubmit}
-          className="edit-form"
-          data-testid="editForm"
-        >
-          <section className="edit-container">
-            <div className="close-container">
-                X
-              <button type="submit" className="close">
-                SAVE
-              </button>
-            </div>
-            <div className="edit-profile"> Edit Profile </div>
-            <div className="input-container">
-              <input
-                type="text"
-                    
-                label="Display name"
-              />
-              <input
-                type="text"
-                label="Age"
-              />
-              <input
-                inputType="textarea"
-                classStyle="input-about"
-                label="About"
-              />
-              <label> Profile Picture </label>
-              <section className="input-image-container">
-              </section>
-              <div className="theme-container">
-                <label> Theme </label>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // onSave({ fullname, email, phone, address });
+  };
+
+  return (
+    <div className="edit-profile ">
+      <div className="edit-container">
+        <div className="edit-profile-title">
+          <h9>EDIT PROFILE</h9>
+          <form onSubmit={handleSubmit}>
+            <div className="form-profile">
+              <div>
+              <FontAwesomeIcon icon={faUser} color="black" />
+                <label htmlFor="fullname">Full Name</label>
                 <input
-                  className="theme-color"
-                  type="color"
+                  type="text"
+                  id="fullname"
+                  placeholder={"Enter your full name"}
+                // value={fullname}
+                // onChange={(event) => setFullname(event.target.value)}
                 />
               </div>
+              <div>
+              <FontAwesomeIcon icon={faEnvelope} color="black" />
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder={"Enter your email"}
+                // value={email}
+                // onChange={(event) => setEmail(event.target.value)}
+                />
+              </div>
+              <div>
+              <FontAwesomeIcon icon={faPhone} color="black" />
+                <label htmlFor="phone">Phone</label>
+                <input
+                  type="text"
+                  id="phone"
+                  pattern="[0-9]*"
+                  maxLength="10"
+                  placeholder={"Enter your phone number"}
+                // value={phone}
+                // onChange={(event) => setPhone(event.target.value)}
+                />
+              </div>
+              <div>
+              <FontAwesomeIcon icon={faLocationDot} color="black" />
+                <label htmlFor="address">Address</label>
+                <input
+                  type="text"
+                  id="address"
+                  placeholder={"Enter your address"}
+                // value={address}
+                // onChange={(event) => setAddress(event.target.value)}
+                />
+              </div>
+              <button type="submit" className="btn-save">Save Changes</button>
             </div>
-          </section>
-        </form>
-        </>
-    );
-  };
-}
-  
-  export default editProfile ;
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default editProfile;
