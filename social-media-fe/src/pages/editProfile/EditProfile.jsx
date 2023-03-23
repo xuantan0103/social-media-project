efb38cf0335994653df2a04bc6d69bfe37f93524
Sương <suongphan37@gmail.com>
2023-03-23 16:55:01 +0700

    call api for edit user

--- social-media-fe/src/pages/editProfile/EditProfile.jsx
+++ social-media-fe/src/pages/editProfile/EditProfile.jsx
@@ -1,4 +1,4 @@
-import "../editProfile/EditProfile.css";
+import "./EditProfile.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
