f450e338c6fa7c978ad1af285352d691936b5a93
XuanTan <tanpham0147@gmail.com>
2023-03-23 14:50:11 +0700

    fix-code-callAPI

--- social-media-fe/src/components/Friends/Friendsuggestion.jsx
+++ social-media-fe/src/components/Friends/Friendsuggestion.jsx
@@ -1,28 +1,35 @@
 import React from "react";
 import styles from "./Friendsuggestion.scss";
 import classNames from "classnames/bind";
-// import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const cx = classNames.bind(styles);
-function Friendsuggestion() {
+export const Button = (props) => {
+  const [isShowaddfr, setIsShowaddfr] = useState(false);
+  const [isShowremove, setIsShowremove] = useState(false);
+const requestAdd = (id) => {
+  setIsShowaddfr(!isShowaddfr)
+  props.addFriend(id)
+}
+const requestRemove = (id) => {
+  setIsShowremove(!isShowremove)
+  props.removeFriend(id)
+}
   return (
     <div className={cx("fr-card")}>
       <div className={cx("fr-card")}>
-        <div className="card" style={{ width: "15rem" }}>
-          <img className="card-img-top" src="https://cdn.pixabay.com/photo/2016/01/02/18/39/puppy-1118584_960_720.jpg" alt="" />
+        <div className="card" style={{ width: "15rem", height:"100%rem" }}>
+          <img className="card-img-top" src="https://i.pinimg.com/564x/44/05/b4/4405b4960b10edd8df4125c26768f852.jpg" alt="" />
           <div className="card-body">
             <h5 className="card-title">Phạm Xuân Tân</h5>
             <p className="card-text">50 bạn chung</p>
-            <a href="#" className="btn btn-primary">
-              Add friend
-            </a>
-            <a href="#" className="btn btn-remove">
-              Remove
-            </a>
+            <button onClick={() => requestAdd(props.id)} className="btn-addfr">{isShowaddfr ? "Invitation sent" : "Addfriend"}</button>
+            <button onClick={() => requestRemove(props.id)} className="btn-remove">{isShowremove ? "Removed" : "Remove"}</button>
+
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
-export default Friendsuggestion;
+export default Button;
--- social-media-fe/src/pages/FriendRequests/FriendRequest.jsx
+++ social-media-fe/src/pages/FriendRequests/FriendRequest.jsx
@@ -1,125 +1,28 @@
-import React, { useState } from "react";
-import { Image, Transformation } from "cloudinary-react";
-import { Link, Route } from "react-router-dom";
+import React from "react";
 import "../FriendRequests/FriendRequest.scss";
-import axios from "axios";
 import Friends from "../../components/Friends/Friend";
+import { Link} from "react-router-dom";
 
-function FriendRequests({ friendsRequest }) {
+function FriendRequest({friendsRequest}){
   const list = [];
   const loading = false;
-  const Button = (props) => {
-    const [isAccept, setIsAccept] = useState("default");
-
-    const onSubmitFriendRequest = async (type, id) => {
-      if (type === "accept") {
-        setIsAccept(true);
-        await axios.post("", { follow_users_UserId: id });
-      } else {
-        setIsAccept(false);
-        await axios.post("", { follow_users_UserId: id });
+    if (!loading && !list.length) {
+        return (
+          <div className="noti-all ">
+            <div className="noti-container">
+              <div className="friend-requests-title">
+                <h4>Your friend request</h4>
+                <Link to="/friendsent">
+                  <span>View submitted requests</span>
+                </Link>
+              </div>
+              <div className="py-2">
+                <Friends />
+              </div>
+            </div>
+          </div>
+        );
       }
-    };
-
-    if (!isAccept) {
-      return (
-        <React.Fragment>
-          <button style={{ backgroundColor: "red", color: "white" }}>
-            Cancelled
-          </button>
-        </React.Fragment>
-      );
-    }
-
-    return (
-      <React.Fragment>
-        {isAccept && isAccept === "default" ? (
-          <>
-            <button
-              style={{ backgroundColor: "rgba(0,128,128)", color: "white" }}
-            >
-              Xác nhận
-            </button>
-            <button
-            >
-              Cancle
-            </button>
-          </>
-        ) : (
-          <button
-            style={{ backgroundColor: "rgba(0,128,128)", color: "white" }}
-          >
-            Friend
-          </button>
-        )}
-      </React.Fragment>
-    );
-  };
-
-  if (!loading && !list.length) {
-    return (
-      <div className="noti-all ">
-        <div className="noti-container">
-          <div className="friend-requests-title">
-            <h4>Your friend request</h4>
-            <Link to="/friendsent">
-              <span>View Sent Requests</span>
-            </Link>
-          </div>
-          <div className="py-2">
-            <Friends />
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  const FriendRequests = () => {
-    return (
-      <>
-        <div className="friends-requests-all" style={{ marginTop: "65px" }}>
-          <div className="friend-requests-container">
-            <div className="friend-requests-title">
-              <h4>Reply {1} Your friend request</h4>
-              <Link to="/friendsent">
-                <span>View Sent Requests</span>
-              </Link>
-            </div>
-            <div className="friend-requests-item-container" key={1}>
-              <div className="friend-requests-item-left">
-                <div className="friend-requests-item-avatar">
-                  <Link to={"/profile/" + "tan"}>
-                    <Image
-                      cloudName="socia-media"
-                      // publicId={rq.user_avatar + ".jpg"}
-                      version="1607061343"
-                    >
-                      <Transformation               
-                      />
-                    </Image>
-                  </Link>
-                </div>
-                <div className="friend-requests-item-info">
-                  <div className="friend-requests-item-name">
-                    <Link to={"/profile/" + "tan"}>Tân</Link>
-                  </div>
-                </div>
-              </div>
-              <div className="friend-requests-item-right">
-                <Button user={""} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  };
-
-  return (
-    <>
-      <Route component={FriendRequests} />
-    </>
-  );
 }
 
-export default FriendRequests;
+export default FriendRequest;
