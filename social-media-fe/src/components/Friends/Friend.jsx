4d503bd40abd7b15e1ce4b3292b905a4c1836b21
XuanTan <tanpham0147@gmail.com>
2023-04-03 16:44:14 +0700

    fix code

--- social-media-fe/src/components/Friends/Friend.jsx
+++ social-media-fe/src/components/Friends/Friend.jsx
@@ -3,18 +3,18 @@ import classNames from "classnames/bind";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import UserImage from "./UserImage";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 // import { friendSlice } from "../../redux/slice/friendSlice";
 import FlexBetween from "./FlexBetween";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { FaUserMinus, FaUserPlus } from "react-icons/fa";
 
 const cx = classNames.bind(styles);
-export const Button = (props, user) => {
+ const Button = (props, user) => {
 /** Contains top part of the post */
 const Friend = ({ id, relation, userPicturePath }) => {
   const dispatch = useDispatch();
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const { _id } = useSelector((state) => state.user);
   const token = useSelector((state) => state.token);
   const friends = useSelector((state) => state.user.friends);
@@ -49,10 +49,10 @@ const Friend = ({ id, relation, userPicturePath }) => {
         {/**User profile photo */}
         <UserImage image={userPicturePath} size="55px" />
         <Box
-          // onClick={() => {
-          //   navigate(`/profile/${id}`);
-          //   navigate(0); //refresh the page from when going into user prof and then going into another prof
-          // }}
+          onClick={() => {
+            navigate(`/profile/${id}`);
+            navigate(0); 
+          }}
         >
             {/**Name */}
             <Typography
