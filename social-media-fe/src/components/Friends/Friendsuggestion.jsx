ba89e4e6321da70dec316684a92ee33063e6c015
XuanTan <tanpham0147@gmail.com>
2023-04-04 15:49:17 +0700

    fix card friend-API

--- social-media-fe/src/components/Friends/Friendsuggestion.jsx
+++ social-media-fe/src/components/Friends/Friendsuggestion.jsx
@@ -4,7 +4,7 @@ import classNames from "classnames/bind";
 import { useState } from "react";
 
 const cx = classNames.bind(styles);
-export const Button = (props) => {
+export const Button = (props,User) => {
   const [isShowaddfr, setIsShowaddfr] = useState(false);
   const [isShowremove, setIsShowremove] = useState(false);
 const requestAdd = (id) => {
@@ -21,7 +21,7 @@ const requestRemove = (id) => {
         <div className="card" style={{ width: "15rem", height:"100%rem" }}>
           <img className="card-img-top" src="https://i.pinimg.com/564x/44/05/b4/4405b4960b10edd8df4125c26768f852.jpg" alt="" />
           <div className="card-body">
-            <h5 className="card-title">Phạm Xuân Tân</h5>
+            <h5 className="card-title">{User.username}</h5>
             <p className="card-text">50 bạn chung</p>
             <button onClick={() => requestAdd(props.id)} className="btn-addfr">{isShowaddfr ? "Send Friend Request" : "Add Friend"}</button>
             <button onClick={() => requestRemove(props.id)} className="btn-remove">{isShowremove ? "Removed Friend" : "Remove Friend"}</button>
