b762c9a33bdd8c60b2d596cb4173738fa702047f
XuanTan <tanpham0147@gmail.com>
2023-04-11 10:08:00 +0700

    delete file-fix code

--- social-media-fe/src/redux/slice/editProfileSlice.js
+++ social-media-fe/src/redux/slice/editProfileSlice.js
@@ -1,43 +1,7 @@
-// import axios from "axios";
-// import {
-//   updateStart,
-//   updateSuccess,
-//   updateError,
-//   getUserStart,
-//   getUserSuccess,
-//   getUserFailed,
-// } from "./userSlice";
-// import { API, BASE_URL } from "../../api/constant";
 
-// export const updateUser = async (user, id, token) => {
-//   updateStart();
-//   try {
-//     const res = await axios.put(`${BASE_URL}/users/${id}`, user, {
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer ${token}`,
-//       },
-//     });
-//     updateSuccess(res.data);
-//   } catch (err) {
-//     console.log(err);
-//     updateError();
-//   }
-// };
-// export const getUser = async (id, token) => {
-//   getUserStart();
-//   try {
-//     const res = await axios.get(`${API}/users/${id}`, {
-//       headers: { "Content-Type": "application/json", token: `Bearer ${token}` },
-//     });
-//     getUserSuccess(res.data);
-//   } catch (err) {
-//     getUserFailed();
-//   }
-// };
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import * as api from "../../api";
-// Action
+//Action
 export const getUserById = createAsyncThunk(
   "user/getUserById",
   async (userId, { rejectWithValue }) => {
